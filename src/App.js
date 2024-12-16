0bbdf46c126bc760e61cc53cfa0cca7ce96c5170
Sajjad Javadi <sajjadjavadi28@gmail.com>
2024-12-16 12:19:55 +0330

    add route about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import MainPage from './pages/Index';
 import {createBrowserRouter, RouterProvider} from "react-router";
+import AboutPage from "./pages/AboutPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainPage/>,
+    },
+    {
+        path: "/about",
+        element: <AboutPage/>,
     },
 ], {});
 
