56e3e027a7ff95467c393ee027a62144fb6c261e
Sajjad Javadi <sajjadjavadi28@gmail.com>
2025-01-17 18:03:36 +0330

    fix icons animation

--- src/components/ContactUs/SupportComponent.jsx
+++ src/components/ContactUs/SupportComponent.jsx
@@ -1,9 +1,11 @@
 import contact3Image from "../../assets/images/contact3_image.png";
 import contact4Image from "../../assets/images/contact4_image.png";
 import contact5Image from "../../assets/images/contact5_image.png";
-import liveChatIcon from "../../assets/svg/contact/live_chat.svg";
-import emailSupportIcon from "../../assets/svg/contact/email_support.svg";
-import phoneSupportIcon from "../../assets/svg/contact/phone_support.svg";
+import liveChat1Icon from "../../assets/svg/contact/live_chat1.svg";
+import liveChat2Icon from "../../assets/svg/contact/live_chat2.svg";
+import emailSupport1Icon from "../../assets/svg/contact/email_support1.svg";
+import emailSupport2Icon from "../../assets/svg/contact/email_support2.svg";
+import phoneSupport2Icon from "../../assets/svg/contact/phone_support2.svg";
 import arrowRightIcon from "../../assets/svg/arrow_right.svg";
 import {useCallback} from "react";
 import {Button} from "@nextui-org/react";
@@ -27,7 +29,12 @@ function SupportComponent() {
                 <div
                     className={"flex items-center justify-center lg:gap-[118px] md:gap-[56px] gap-[40px] flex-col md:flex-row"}>
                     <div className={"max-w-[520px] flex items-start justify-center gap-4 flex-col "}>
-                        <img src={liveChatIcon} alt={"about1"} className={""}/>
+                        <div className={"relative group mb-4"}>
+                            <img src={liveChat1Icon} alt={"liveChat"}
+                                 className={"group-hover:-rotate-6 transition-all"}/>
+                            <img src={liveChat2Icon} alt={"liveChat"}
+                                 className={"group-hover:rotate-6 transition-all absolute -bottom-4 -right-1/2 -translate-x-[10%]"}/>
+                        </div>
                         <h3 className={"roboto text-4xl font-semibold"}>Live Chat</h3>
                         <p className={"font-plusJakartaSans text500 text-base"}>We provide 24/7 chat support accessible
                             via the website and app for real-time assistance with account issues, foreign exchange
@@ -45,16 +52,27 @@ function SupportComponent() {
                     className={"flex items-center justify-center lg:gap-[118px] md:gap-[56px] gap-[40px] flex-col md:flex-row"}>
                     <img src={contact4Image} alt={"about1"} className={"w-[520px] "}/>
                     <div className={"max-w-[520px] flex items-start justify-center gap-4 flex-col "}>
-                        <img src={emailSupportIcon} alt={"about1"} className={""}/>
+                        <div className={"relative group mb-4"}>
+                            <img src={emailSupport1Icon} alt={"email support"}
+                                 className={"group-hover:-rotate-6 transition-all"}/>
+                            <img src={emailSupport2Icon} alt={"email support"}
+                                 className={"group-hover:rotate-6 transition-all absolute -bottom-4 -right-1/2 -translate-x-[10%]"}/>
+                        </div>
                         <h3 className={"roboto text-4xl font-semibold"}>Email Support</h3>
-                        <p className={"font-plusJakartaSans text500 text-base"}> offer <a href="mailto:info@keyfx.co.uk"><u>info@keyfx.co.uk</u></a> with a
+                        <p className={"font-plusJakartaSans text500 text-base"}> offer <a
+                            href="mailto:info@keyfx.co.uk"><u>info@keyfx.co.uk</u></a> with a
                             quick response time for general questions, document submissions, and feedback.</p>
                     </div>
                 </div>
                 <div
                     className={"flex items-center justify-center lg:gap-[118px] md:gap-[56px] gap-[40px] flex-col md:flex-row"}>
                     <div className={"max-w-[520px] flex items-start justify-center gap-4 flex-col "}>
-                        <img src={phoneSupportIcon} alt={"about1"} className={""}/>
+                        <div className={"relative group mb-4"}>
+                            <img src={emailSupport1Icon} alt={"phone support"}
+                                 className={"group-hover:-rotate-6 transition-all"}/>
+                            <img src={phoneSupport2Icon} alt={"phone support"}
+                                 className={"group-hover:rotate-6 transition-all absolute -bottom-4 -right-1/2 -translate-x-[10%]"}/>
+                        </div>
                         <h3 className={"roboto text-4xl font-semibold"}>Phone Support</h3>
                         <p className={"font-plusJakartaSans text500 text-base"}>Include a customer service number,
                             operational during business hours, for immediate assistance with urgent account matters.</p>
